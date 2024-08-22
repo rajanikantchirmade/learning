13dbcf74842ca39c63648e54fe07c560f75392d2
Rajanikant Chirmade <rajanikant.chirmade@seagate.com>
2024-08-22 16:29:48 +0000

    Used for loop for ascent and descend

--- helloworld/loop.c
+++ helloworld/loop.c
@@ -5,7 +5,14 @@ int main()
     int count = 10;
     int i;
 
+    printf("Print ascending ...\n");
     for (i = 0; i< count; i++)
+    {
+        printf("Count : %d\n", i);
+    }
+
+    printf("Print Descent ...\n");
+    for (i = count; i > 0; i--)
     {
         printf("Count : %d\n", i);
     }
